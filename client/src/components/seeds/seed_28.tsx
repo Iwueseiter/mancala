1d1048ccda2e5054df30db31416e68001455432c
supreme2580 <victoromorogbe69@gmail.com>
2024-10-03 21:13:30 +0100

    seeds definition

---
+++ client/src/components/seeds/seed_28.tsx
@@ -0,0 +1,10 @@
+export default function SeedTwentyEight({ color, zIndex }: { color: string; zIndex: number }) {
+    return(
+        <div
+            className={`w-[16px] h-[16px] bg-center bg-cover bg-no-repeat ${
+                color === "Green" ? "bg-[url('./assets/green-seed.png')]" : "bg-[url('./assets/purple-seed.png')]"
+            }`}
+            style={{ zIndex }}
+        />
+    )
+}
